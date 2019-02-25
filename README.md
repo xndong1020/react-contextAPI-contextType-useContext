2913667fbde5a7370a1a7e2a4bc5a7a0743d5ae9
Jeremy Gu <isdance2004@hotmail.com>
2019-02-25 22:49:48 +1100

    convert static contextType to useContext hook

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 The project will demo the evolution of context API -> contextType -> useContext
 
-useContext will be in master branch
+useContext will be in useContext branch
 contextType will be in contextType branch
 context API will be in contextAPI branch
 
--- src/Components/GrandChild.jsx
+++ src/Components/GrandChild.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import UserContext from '../context/UserContext'
 
-export default class GrandChild extends Component {
-  static contextType = UserContext
-
-  componentDidMount() {
-    console.log('I have access to context now', this.context.users)
-  }
-  render() {
-    return (
-      <p>
-        This is grand child component, I also can see there are{' '}
-        {this.context.users.length} users in the context
-      </p>
-    )
-  }
+const GrandChild = props => {
+  const userContext = useContext(UserContext)
+  return (
+    <p>
+      This is grand child component, I also can see there are{' '}
+      {userContext.users.length} users in the context
+    </p>
+  )
 }
+
+export default GrandChild
