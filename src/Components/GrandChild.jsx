7a1f6109a97a0d2fd198b86981863a5b1309367d
Jeremy Gu <isdance2004@hotmail.com>
2019-02-25 22:34:03 +1100

    Convert contextAPI consumer to static contextType

--- src/Components/GrandChild.jsx
+++ src/Components/GrandChild.jsx
@@ -2,20 +2,17 @@ import React, { Component } from 'react'
 import UserContext from '../context/UserContext'
 
 export default class GrandChild extends Component {
+  static contextType = UserContext
+
+  componentDidMount() {
+    console.log('I have access to context now', this.context.users)
+  }
   render() {
     return (
-      <UserContext.Consumer>
-        {({ users }) => {
-          return (
-            <div>
-              <p>
-                This is grand child component, I also can see there are{' '}
-                {users.length} users in the context
-              </p>
-            </div>
-          )
-        }}
-      </UserContext.Consumer>
+      <p>
+        This is grand child component, I also can see there are{' '}
+        {this.context.users.length} users in the context
+      </p>
     )
   }
 }
